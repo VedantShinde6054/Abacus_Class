c18b6115843fd1b83d733f3d8541190e9dcde20e
unknown <vedantjshinde@gmail.com>
2024-03-17 14:54:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hi vedant
+          Learn React hi vedant shinde
         </a>
       </header>
     </div>
