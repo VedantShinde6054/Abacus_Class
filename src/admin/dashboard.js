4e635381eead0e8bbf6f837d6def24f49829e716
Mohit Khambekar <mohitkhambekar@gmail.com>
2024-04-13 00:46:56 +0530

    admin page without css added

--- src/admin/dashboard.js
+++ src/admin/dashboard.js
@@ -1,137 +1,219 @@
 import { useEffect, useState } from "react";
 import Menu from "../components/Menu.js";
-import Sidebar from "./sidebar";
 import "./dashboard.css";
 
-const Dashboard = () => {
-  const [record, setRecord] = useState([]);
-  const getData = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((resposne) => resposne.json())
-      .then((res) => setRecord(res));
-  };
-  useEffect(() => {
-    getData();
-  });
-
+function Dashboard() {
   return (
     <>
       <Menu></Menu>
-      <section className="admin-container">
-      <Sidebar></Sidebar>
-       <div className="row">
-      <center>
-          <div class="col main pt-5 mt-3">
-            <p class="lead d-none d-sm-block">
-              Add Employee Details and Records
-            </p>
-            <div
-              class="alert alert-warning fade collapse"
-              role="alert"
-              id="myAlert"
-            >
-              <button
-                type="button"
-                class="close"
-                data-dismiss="alert"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">×</span>
-                <span class="sr-only">Close</span>
-              </button>
-              <strong>Data and Records</strong> Learn more about employee
+      <div class="side-menu">
+        <div class="brand-name">
+          <h1>Brand</h1>
+        </div>
+        <ul>
+          <li>
+            <img src="dashboard (2).png" alt="" />
+            &nbsp; <span>Dashboard</span>{" "}
+          </li>
+          <li>
+            <img src="reading-book (1).png" alt="" />
+            &nbsp;<span>Admission Requests</span>{" "}
+          </li>
+          <li>
+            <img src="teacher2.png" alt="" />
+            &nbsp;<span>Teachers Requests</span>{" "}
+          </li>
+          <li>
+            <img src="school.png" alt="" />
+            &nbsp;<span>Add Users</span>{" "}
+          </li>
+          <li>
+            <img src="payment.png" alt="" />
+            &nbsp;<span>Content Manager</span>{" "}
+          </li>
+          <li>
+            <img src="help-web-button.png" alt="" />
+            &nbsp; <span>Help</span>
+          </li>
+          <li>
+            <img src="settings.png" alt="" />
+            &nbsp;<span>Settings</span>{" "}
+          </li>
+        </ul>
+      </div>
+      <div class="container-dashboard">
+        <div class="content">
+          <div class="cards">
+            <div class="card">
+              <div class="box">
+                <h1>2194</h1>
+                <h3>Students</h3>
+              </div>
+              <div class="icon-case">
+                <img src="students.png" alt="" />
+              </div>
+            </div>
+            <div class="card">
+              <div class="box">
+                <h1>53</h1>
+                <h3>Teachers</h3>
+              </div>
+              <div class="icon-case">
+                <img src="teachers.png" alt="" />
+              </div>
+            </div>
+            <div class="card">
+              <div class="box">
+                <h1>5</h1>
+                <h3>Schools</h3>
+              </div>
+              <div class="icon-case">
+                <img src="schools.png" alt="" />
+              </div>
+            </div>
+            <div class="card">
+              <div class="box">
+                <h1>350000</h1>
+                <h3>Income</h3>
+              </div>
+              <div class="icon-case">
+                <img src="income.png" alt="" />
+              </div>
             </div>
           </div>
-
-          <div class="row mb-3">
-            <div class="col-xl-3 col-sm-6 py-2">
-              <div class="card bg-success text-white h-100">
-                <div
-                  class="card-body bg-success"
-                  style={{ backgroundColor: "#57b960" }}
-                >
-                  <div class="rotate">
-                    <i class="fa fa-user fa-4x"></i>
-                  </div>
-                  <h6 class="text-uppercase">Users</h6>
-                  <h1 class="display-4">134</h1>
-                </div>
+          <div class="content-2">
+            <div class="recent-payments">
+              <div class="title">
+                <h2>Recent Payments</h2>
+                <a href="#" class="btn">
+                  View All
+                </a>
               </div>
+              <table>
+                <tr>
+                  <th>Name</th>
+                  <th>School</th>
+                  <th>Amount</th>
+                  <th>Option</th>
+                </tr>
+                <tr>
+                  <td>John Doe</td>
+                  <td>St. James College</td>
+                  <td>$120</td>
+                  <td>
+                    <a href="#" class="btn">
+                      View
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>John Doe</td>
+                  <td>St. James College</td>
+                  <td>$120</td>
+                  <td>
+                    <a href="#" class="btn">
+                      View
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>John Doe</td>
+                  <td>St. James College</td>
+                  <td>$120</td>
+                  <td>
+                    <a href="#" class="btn">
+                      View
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>John Doe</td>
+                  <td>St. James College</td>
+                  <td>$120</td>
+                  <td>
+                    <a href="#" class="btn">
+                      View
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>John Doe</td>
+                  <td>St. James College</td>
+                  <td>$120</td>
+                  <td>
+                    <a href="#" class="btn">
+                      View
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>John Doe</td>
+                  <td>St. James College</td>
+                  <td>$120</td>
+                  <td>
+                    <a href="#" class="btn">
+                      View
+                    </a>
+                  </td>
+                </tr>
+              </table>
             </div>
-            <div class="col-xl-3 col-sm-6 py-2">
-              <div class="card text-white bg-danger h-100">
-                <div class="card-body bg-danger">
-                  <div class="rotate">
-                    <i class="fa fa-list fa-4x"></i>
-                  </div>
-                  <h6 class="text-uppercase">Posts</h6>
-                  <h1 class="display-4">87</h1>
-                </div>
+            <div class="new-students">
+              <div class="title">
+                <h2>New Students</h2>
+                <a href="#" class="btn">
+                  View All
+                </a>
               </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 py-2">
-              <div class="card text-white bg-info h-100">
-                <div class="card-body bg-info">
-                  <div class="rotate">
-                    <i class="fab fa-twitter fa-4x"></i>
-                  </div>
-                  <h6 class="text-uppercase">Tweets</h6>
-                  <h1 class="display-4">125</h1>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 py-2">
-              <div class="card text-white bg-warning h-100">
-                <div class="card-body">
-                  <div class="rotate">
-                    <i class="fa fa-share fa-4x"></i>
-                  </div>
-                  <h6 class="text-uppercase">Shares</h6>
-                  <h1 class="display-4">36</h1>
-                </div>
-              </div>
+              <table>
+                <tr>
+                  <th>Profile</th>
+                  <th>Name</th>
+                  <th>option</th>
+                </tr>
+                <tr>
+                  <td>
+                    <img src="user.png" alt="" />
+                  </td>
+                  <td>John Steve Doe</td>
+                  <td>
+                    <img src="info.png" alt="" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src="user.png" alt="" />
+                  </td>
+                  <td>John Steve Doe</td>
+                  <td>
+                    <img src="info.png" alt="" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src="user.png" alt="" />
+                  </td>
+                  <td>John Steve Doe</td>
+                  <td>
+                    <img src="info.png" alt="" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src="user.png" alt="" />
+                  </td>
+                  <td>John Steve Doe</td>
+                  <td>
+                    <img src="info.png" alt="" />
+                  </td>
+                </tr>
+              </table>
             </div>
           </div>
-        </center>
-          </div>
-        <hr />
-
-          <div class="row ">
-            <center>
-              <div class="col-lg-7 col-md-6 col-sm-12">
-                <h5 class="mt-3 mb-3 text-secondary">
-                  Check More Records of Employees
-                </h5>
-                <div class="table-responsive">
-                  <table class="table table-striped">
-                    <thead class="thead-light">
-                      <tr>
-                        <th>No</th>
-                        <th>Label</th>
-                        <th>Header</th>
-                        <th>Column</th>
-                        <th>Record Data</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {record.slice(0, 5).map((output) => (
-                        <tr>
-                          <td>{output.id}</td>
-                          <td>{output.name}</td>
-                          <td>{output.email}</td>
-                          <td>{output.username}</td>
-                          <td>{output.website}</td>
-                          <td></td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </center>
-          </div>
-      </section>
+        </div>
+      </div>
     </>
   );
-};
+}
+
 export default Dashboard;
