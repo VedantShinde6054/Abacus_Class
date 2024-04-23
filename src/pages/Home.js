61fdbdd66faf3efdc76f25b30e75a6c9d34a633c
Mohit Khambekar <mohitkhambekar@gmail.com>
2024-04-24 02:07:52 +0530

    Home Page updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import "../assets/css/Home.css";
-import bgimg from "../assets/images/back-4.jpg";
 import Menu from "../components/Menu";
 import Footer from "../components/footer";
 import Cards1 from "../components/cards";
@@ -18,253 +17,208 @@ function Index() {
       <Menu />
 
       <section className="hero-section">
-        <div
-          className="back"
-          style={{
-            backgroundImage: `url(${bgimg})`,
-          }}
-        >
-          <div className="d-flex align-items-center justify-content-center fs-1">
-            <div className="container">
-              <div id="carouselExampleIndicators" className="carousel slide">
-                <div className="carousel-indicators">
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="0"
-                    className="active"
-                    aria-current="true"
-                    aria-label="Slide 1"
-                  ></button>
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="1"
-                    aria-label="Slide 2"
-                  ></button>
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="2"
-                    aria-label="Slide 3"
-                  ></button>
+        <div className="back-filter">
+          <div className="carousal-space  align-items-center justify-content-center">
+            <div
+              id="carouselExampleAutoplaying"
+              class="carousel slide "
+              data-bs-ride="carousel"
+            >
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    src={require("../assets/images/template1.jpg")}
+                    className="d-block w-100 object-fit-xl-fill border rounded corr-img1"
+                    alt="..."
+                  />
+                  <div className="black-effect"></div>
+                  <img
+                    src={require("../assets/images/template1.jpg")}
+                    className="d-block w-100 object-fit-xl-contain border rounded corr-img2"
+                    alt="..."
+                  />
                 </div>
-                <div className="carousel-inner">
-                  <div className="carousel-item active">
-                    <img
-                      src={require("../assets/images/template1.jpg")}
-                      className="d-block w-100 object-fit-xl-fill border rounded corr-img1"
-                      alt="..."
-                    />
-                    <div className="black-effect"></div>
-                    <img
-                      src={require("../assets/images/template1.jpg")}
-                      className="d-block w-100 object-fit-xl-contain border rounded corr-img2"
-                      alt="..."
-                    />
-                  </div>
-
-                  <div className="carousel-item">
-                    <img
-                      src={require("../assets/images/template2.jpg")}
-                      // className="d-block w-100"
-                      className="d-block w-100 object-fit-xl-fill border rounded corr-img1"
-                      alt="..."
-                    />
-                    <div className="black-effect"></div>
-                    <img
-                      src={require("../assets/images/template2.jpg")}
-                      // className="d-block w-100"
-                      className="d-block w-100 object-fit-xl-contain border rounded corr-img2"
-                      alt="..."
-                    />
-                  </div>
-
-                  <div className="carousel-item">
-                    <img
-                      src={require("../assets/images/ISO_Certificate.jpg")}
-                      // className="d-block w-100"
-                      className="d-block w-100 object-fit-xl-fill border rounded corr-img1"
-                      alt="..."
-                    />
-                    <div className="black-effect"></div>
-                    <img
-                      src={require("../assets/images/ISO_Certificate.jpg")}
-                      // className="d-block w-100"
-                      className="d-block w-100 object-fit-xl-contain border rounded corr-img2"
-                      alt="..."
-                    />
-                  </div>
+                <div class="carousel-item">
+                  <img
+                    src={require("../assets/images/template2.jpg")}
+                    className="d-block w-100 object-fit-xl-fill border rounded corr-img1"
+                    alt="..."
+                  />
+                  <div className="black-effect"></div>
+                  <img
+                    src={require("../assets/images/template2.jpg")}
+                    className="d-block w-100 object-fit-xl-contain border rounded corr-img2"
+                    alt="..."
+                  />
                 </div>
-                <button
-                  className="carousel-control-prev"
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide="prev"
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                  className="carousel-control-next"
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide="next"
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Next</span>
-                </button>
+                <div class="carousel-item">
+                  <img
+                    src={require("../assets/images/ISO_Certificate.jpg")}
+                    className="d-block w-100 object-fit-xl-fill border rounded corr-img1"
+                    alt="..."
+                  />
+                  <div className="black-effect"></div>
+                  <img
+                    src={require("../assets/images/ISO_Certificate.jpg")}
+                    className="d-block w-100 object-fit-xl-contain border rounded corr-img2"
+                    alt="..."
+                  />
+                </div>
               </div>
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+              </button>
             </div>
           </div>
+
           <div class=" text-center container-content">
             <div class="row">
               <h1>About Abacus</h1>
             </div>
-            <div class="row ab-info">
-              <div class="col ab-info-txt ">
-                <h4>
-                  {" "}
-                  <span id="span1">What is ABACUS ?</span>{" "}
-                </h4>
-                <strong>The Abacus</strong> is a historic calculation tool with
-                rods and beads that represent numbers when slid along the rods.
-                It's utilized in modern times to enhance mental math skills,
-                particularly in children aged 5 to 14. <br /> It facilitates
-                various arithmetic operations, including addition, subtraction,
-                multiplication, and division, as well as decimal calculations
-                and handling negative numbers.
+            <div className="bg-space">
+              <div class="row ab-info">
+                <div class="col ab-info-txt ">
+                  <h4>
+                    {" "}
+                    <span id="span1">What is ABACUS ?</span>{" "}
+                  </h4>
+                  <strong>The Abacus</strong> is a historic calculation tool
+                  with rods and beads that represent numbers when slid along the
+                  rods. It's utilized in modern times to enhance mental math
+                  skills, particularly in children aged 5 to 14. <br /> It
+                  facilitates various arithmetic operations, including addition,
+                  subtraction, multiplication, and division, as well as decimal
+                  calculations and handling negative numbers.
+                </div>
+                <div class="col ab-info-img">
+                  <img
+                    src={require("../assets/images/abacus.png")}
+                    className="advan1"
+                    alt="..."
+                  />
+                </div>
               </div>
-              <div class="col ab-info-img">
-                <img
-                  src={require("../assets/images/advantages1.png")}
-                  className="advan1"
-                  alt="..."
-                />
+
+              <div class="row ab-info">
+                <div class="col ab-info-img">
+                  <img
+                    src={require("../assets/images/maths.png")}
+                    className="mental1"
+                    alt=".."
+                  />
+                </div>
+                <div class="col ab-info-txt ">
+                  <h4>What is Mental Arithmetic ?</h4>
+                  <li>
+                    Mental Arithmetic is a term used in Abacus for the
+                    calculations done in mind.
+                  </li>
+                  <li>
+                    Even large numbers can be calculated in mind, that too
+                    within seconds.
+                  </li>{" "}
+                  <li>
+                    Mental arithmetic improves student's imagination skills
+                    Effective in cracking competitive time based tests.
+                  </li>{" "}
+                  <li>
+                    Mind calculation enhances the concentration level of child.
+                  </li>
+                </div>
               </div>
             </div>
 
-            <div class="row ab-info">
-              <div class="col ab-info-img">
-                <img
-                  src={require("../assets/images/mentalArithematics.png")}
-                  className="mental1"
-                  alt=".."
-                />
-              </div>
-              <div class="col ab-info-txt ">
-                <h4>What is Mental Arithmetic ?</h4>
-                <li>
-                  Mental Arithmetic is a term used in Abacus for the
-                  calculations done in mind.
-                </li>
-                <li>
-                  Even large numbers can be calculated in mind, that too within
-                  seconds.
-                </li>{" "}
-                <li>
-                  Mental arithmetic improves student's imagination skills
-                  Effective in cracking competitive time based tests.
-                </li>{" "}
-                <li>
-                  Mind calculation enhances the concentration level of child.
-                </li>
-              </div>
-            </div>
-
-            {/* <div class="row benifites">
-              <div class="col benifites-txt ">
-                <h4>Benifites of Abacus :-</h4>
-                <li>The process improves the listening ability</li>
-                <li>It Enhances the level of concentration</li>
-                <li>Develops the ability to visualize</li>
-                <li>Builds photographic memory</li>
-                <li>Enables perfect recall</li>
-                <li>The child becomes confident</li>
-                <li>Promotes Logical Thinking</li>
-              </div>
-              <div className=" col benifites-img">
-                <img
-                  src={require("../assets/images/abacus-advantages .png")}
-                  className="benifites1"
-                  alt=".."
-                />
-              </div>
-            </div> */}
             <div class="row">
               <h1>What we Offer</h1>
             </div>
-            <div class="row">
-              <div class="col">
-                <center>
-                  <Cards1
-                    imgSrc={feature1}
-                    altimg="Features of classes"
-                    ctext="Abacus Calculations up to 5 digit numbers."
-                  />
-                </center>
+            <div className="bg-space">
+              <div class="row">
+                <div class="col">
+                  <center>
+                    <Cards1
+                      imgSrc={feature1}
+                      altimg="Features of classes"
+                      ctext="Abacus Calculations up to 5 digit numbers."
+                    />
+                  </center>
+                </div>
+                <div class="col">
+                  <center>
+                    <Cards1
+                      imgSrc={feature2}
+                      altimg="Features of classes"
+                      ctext="Mentorship support for students."
+                    />
+                  </center>
+                </div>
+                <div class="col">
+                  <center>
+                    <Cards1
+                      imgSrc={feature3}
+                      altimg="Features of classes"
+                      ctext="Semi-Olympiyad Exam pratice."
+                    />
+                  </center>
+                </div>
               </div>
-              <div class="col">
-                <center>
-                  <Cards1
-                    imgSrc={feature2}
-                    altimg="Features of classes"
-                    ctext="Mentorship support for students."
-                  />
-                </center>
-              </div>
-              <div class="col">
-                <center>
-                  <Cards1
-                    imgSrc={feature3}
-                    altimg="Features of classes"
-                    ctext="Semi-Olympiyad Exam pratice."
-                  />
-                </center>
-              </div>
-            </div>
 
-            <div class="row">
-              <div class="col ">
-                <center>
-                  <Cards1
-                    imgSrc={feature4}
-                    altimg="Features of classes"
-                    ctext="Test series for students and Teachers."
-                  />
-                </center>
-              </div>
-              <div class="col ">
-                <center>
-                  <Cards1
-                    imgSrc={feature5}
-                    altimg="Features of classes"
-                    ctext="Best Faculty for Teaching and Mentoring."
-                  />
-                </center>
-              </div>
-              <div class="col">
-                <center>
-                  <Cards1
-                    imgSrc={feature6}
-                    altimg="Features of classes"
-                    ctext="Doubt solving session on every Sunday."
-                  />
-                </center>
+              <div class="row">
+                <div class="col ">
+                  <center>
+                    <Cards1
+                      imgSrc={feature4}
+                      altimg="Features of classes"
+                      ctext="Test series for students and Teachers."
+                    />
+                  </center>
+                </div>
+                <div class="col ">
+                  <center>
+                    <Cards1
+                      imgSrc={feature5}
+                      altimg="Features of classes"
+                      ctext="Best Faculty for Teaching and Mentoring."
+                    />
+                  </center>
+                </div>
+                <div class="col">
+                  <center>
+                    <Cards1
+                      imgSrc={feature6}
+                      altimg="Features of classes"
+                      ctext="Doubt solving session on every Sunday."
+                    />
+                  </center>
+                </div>
               </div>
             </div>
           </div>
+
+          <Floatingbutton />
+
+          <Footer />
         </div>
-
-        <Floatingbutton />
-
-        <Footer />
       </section>
     </Fragment>
   );
