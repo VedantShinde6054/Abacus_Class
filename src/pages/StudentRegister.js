c5e2b25e217f8130cc374125537876dab28f6578
vedant shinde <shindevedant595@gmail.com>
2024-04-25 00:19:54 +0530

    Some Css is Changed

--- src/pages/StudentRegister.js
+++ src/pages/StudentRegister.js
@@ -41,98 +41,99 @@ function StudentRegister() {
         <div>
           <Menu />
         </div>
-        <main className="reg-main">
-          <header className="headerR">
-            <center>
-              <h3>Student Registration</h3>
-            </center>
-          </header>
+        <div className="register-main">
+          <main className="reg-main">
+            <header className="headerR">
+              <center>
+                <h3>Student Registration</h3>
+              </center>
+            </header>
 
-          <form className="formR">
-            <div className="form_wrapperR">
-              <span className="reg-title">Student Name :</span>
-              <input
-                id="inputR"
-                type="text"
-                placeholder="Enter Your Fullname "
-                required
-                onChange={(event) => {
-                  setName(event.target.value);
-                }}
-              />
-            </div>
+            <form className="formR">
+              <div className="form_wrapperR">
+                <span className="reg-title">Student Name :</span>
+                <input
+                  id="inputR"
+                  type="text"
+                  placeholder="Enter Your Fullname "
+                  required
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                />
+              </div>
 
-            <div className="form_wrapperR">
-              <span className="reg-title">Student DOB :</span>
-              <input
-                id="dateR"
-                type="date"
-                required
-                onChange={(event) => {
-                  setDob(event.target.value);
-                }}
-              />
-            </div>
+              <div className="form_wrapperR">
+                <span className="reg-title">Student DOB :</span>
+                <input
+                  id="dateR"
+                  type="date"
+                  required
+                  onChange={(event) => {
+                    setDob(event.target.value);
+                  }}
+                />
+              </div>
 
-            <div className="form_wrapperR">
-              <span className="reg-title">Student Gender :</span>
-              <input
-                class="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                value="Male"
-                onChange={(event) => {
-                  setGender(event.target.value);
-                }}
-              />
-              &emsp;
-              <span class="reg-title" for="flexRadioDefault2">
-                Male
-              </span>
-              <input
-                class="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                value="Female"
-                onChange={(event) => {
-                  setGender(event.target.value);
-                }}
-              />
-              &emsp;
-              <span class="reg-title" for="flexRadioDefault2">
-                Female
-              </span>
-            </div>
+              <div className="form_wrapperR">
+                <span className="reg-title">Student Gender :</span>
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="flexRadioDefault"
+                  value="Male"
+                  onChange={(event) => {
+                    setGender(event.target.value);
+                  }}
+                />
+                &emsp;
+                <span class="reg-title" for="flexRadioDefault2">
+                  Male
+                </span>
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="flexRadioDefault"
+                  value="Female"
+                  onChange={(event) => {
+                    setGender(event.target.value);
+                  }}
+                />
+                &emsp;
+                <span class="reg-title" for="flexRadioDefault2">
+                  Female
+                </span>
+              </div>
 
-            <div className="form_wrapperR">
-              <span className="reg-title">Student Email :</span>
-              <input
-                id="emailR"
-                type="email"
-                placeholder="Enter Valid Email"
-                required
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-              />
-            </div>
+              <div className="form_wrapperR">
+                <span className="reg-title">Student Email :</span>
+                <input
+                  id="emailR"
+                  type="email"
+                  placeholder="Enter Valid Email"
+                  required
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                />
+              </div>
 
-            <div className="form_wrapperR">
-              <span className="reg-title">Student Contact :</span>
-              <input
-                type="tel"
-                placeholder="Phone No."
-                name="Phone"
-                pattern="[0-9]{10}"
-                id="phoneR"
-                required
-                onChange={(event) => {
-                  setPhone(event.target.value);
-                }}
-              />
-            </div>
+              <div className="form_wrapperR">
+                <span className="reg-title">Student Contact :</span>
+                <input
+                  type="tel"
+                  placeholder="Phone No."
+                  name="Phone"
+                  pattern="[0-9]{10}"
+                  id="phoneR"
+                  required
+                  onChange={(event) => {
+                    setPhone(event.target.value);
+                  }}
+                />
+              </div>
 
-            {/* <div className="form_wrapperR">
+              {/* <div className="form_wrapperR">
               <span className="reg-title">Student Username :</span>
               <input
                 id="nameR"
@@ -143,28 +144,29 @@ function StudentRegister() {
               <i className="material-icons">person</i>
             </div> */}
 
-            <div className="form_wrapperR">
-              <span className="reg-title">Student Password :</span>
-              <input
-                id="passwordR"
-                type="password"
-                required
-                placeholder="Set Password"
-              />
-              {/* <i className="material-icons">lock</i> */}
-            </div>
-            <div className="remember_box">
-              <div className="remember">
-                <input type="checkbox" />I agree to register for this course
+              <div className="form_wrapperR">
+                <span className="reg-title">Student Password :</span>
+                <input
+                  id="passwordR"
+                  type="password"
+                  required
+                  placeholder="Set Password"
+                />
+                {/* <i className="material-icons">lock</i> */}
               </div>
-            </div>
-            <center>
-              <button className="buttonR" onClick={createUser}>
-                Register
-              </button>
-            </center>
-          </form>
-        </main>
+              <div className="remember_box">
+                <div className="remember">
+                  <input type="checkbox" />I agree to register for this course
+                </div>
+              </div>
+              <center>
+                <button className="buttonR" onClick={createUser}>
+                  Register
+                </button>
+              </center>
+            </form>
+          </main>
+        </div>
       </div>
       <Footer />
     </Fragment>
